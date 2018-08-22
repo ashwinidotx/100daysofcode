9d7e157425fb0ba06abd9253066e1ab21a662b73
ashwinidotx <ashwinix@icloud.com>
2018-08-22 17:03:48 +0530

    JDBC

---
+++ Files/Day 7/jdbcOne.java
@@ -0,0 +1,36 @@
+
+import java.sql.*;
+
+public class jdbcOne{
+    public static void main(String[] args) throws Exception
+    {
+        String url="jdbc:mysql://localhost/YourDatabaseName";
+        String user="root";
+        String password="yourPassword";
+
+        String query1="select * from student"; // student- your table name.
+        String query2="select name from student where branch='CSE'";
+
+        Class.forName("com.mysql.jdbc.Driver");
+        Connection conn=DriverManager.getConnection(url,user,password);
+        Statement st=conn.createStatement();
+        ResultSet rSet=st.executeQuery(query1);
+
+        while(rSet.next())
+        {
+            int id=rSet.getInt("student_id");
+            String name=rSet.getString("name");
+            String branch=rSet.getString("branch");
+            System.out.println("Student ID: "+id+" Name: "+name+" Branch: "+branch);
+        }
+
+        rSet.beforeFirst();
+        rSet=st.executeQuery(query2);
+        while(rSet.next())
+        {
+            int id=rSet.getInt("student_id");
+            String name=rSet.getString("name");
+            System.out.println("Student ID: "+id+" Name: "+name);
+        }
+    }
+}
