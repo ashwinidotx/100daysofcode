48dfa2739a1812b31137bc532f8ce6ce004fea9d
ashwinidotx <ashwinix@icloud.com>
2018-10-16 09:50:55 +0530

    More with Threads - java

--- Files/Day 30/Thread_Simplified.java
+++ Files/Day 30/Thread_Simplified.java
@@ -26,15 +26,30 @@ public class Thread_Simplified {
 				catch(Exception e) {System.out.println(e);}
 			}
 		});
-		
+		// Setting name.
+		t1.setName("Current Value");
+		t2.setName("Remainder Value");
+		String thread1 = t1.getName(), thread2 = t2.getName();
+
+		// Setting priority.
+		System.out.println("Priority:");
+		System.out.println(thread1 + " : " + t1.getPriority());
+		System.out.println(thread2 + " : " + t2.getPriority());
+		System.out.println();
+
+		t1.setPriority(Thread.MIN_PRIORITY);
+		t2.setPriority(Thread.MAX_PRIORITY);
+
+		// executing threads.
 		t1.start();
 		try {
 			Thread.sleep(100);
 		}
 		catch(Exception e) {System.out.println(e);}
 		t2.start();
-		System.out.println("t1 status: alive:"+t1.isAlive());
-		System.out.println("t2 status: alive:" + t2.isAlive());
+		System.out.println(thread1 + "'s status: Alive: " + t1.isAlive());
+		System.out.println(thread2 + "'s status: Alive: " + t2.isAlive());
+
 		System.out.println("End Of Program.\nBye !!!");
 
 	} //End of Main
